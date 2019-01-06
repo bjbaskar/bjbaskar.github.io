c2a203d3120ba6912938638a51f1cbc722697e31
Baskaran Balaganesan <bjbaskar@yahoo.com>
2019-01-06 20:49:22 +0530

    Add files via upload

---
+++ precache-manifest.3f5e4564f0564ce6ae795c932d87da99.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "37fc1be7db8411f67842db47766ceae3",
+    "url": "/static/media/bjbaskar2.37fc1be7.jpg"
+  },
+  {
+    "revision": "9accfebd59366a9c8162",
+    "url": "/static/css/main.636dd2ce.chunk.css"
+  },
+  {
+    "revision": "cdce4977f699f7ca4f6a",
+    "url": "/static/js/1.cdce4977.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e3c63fe1f65679b4c5e797001541737",
+    "url": "/static/media/wordcloud.2e3c63fe.png"
+  },
+  {
+    "revision": "9accfebd59366a9c8162",
+    "url": "/static/js/main.9accfebd.chunk.js"
+  },
+  {
+    "revision": "bff7e8e64dfb35e87d38e1caf73f19a0",
+    "url": "/static/media/LKY_SG.bff7e8e6.jpg"
+  },
+  {
+    "revision": "c84701231002c0e8dda6582f5ba8beb8",
+    "url": "/static/media/linkedin.c8470123.svg"
+  },
+  {
+    "revision": "0c119a35c0588edeaa0b5c9c7ef74378",
+    "url": "/static/media/twitter-logo.0c119a35.svg"
+  },
+  {
+    "revision": "137c40f0381d980e4311b59f114c7293",
+    "url": "/static/media/github-logo.137c40f0.svg"
+  },
+  {
+    "revision": "18d4e202c425d2b76ae924539a958807",
+    "url": "/index.html"
+  }
+];
