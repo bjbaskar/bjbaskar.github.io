fe872c3dc9eddbe87bd16f0cd45a54ccb853ccfc
Baskaran Balaganesan <bjbaskar@yahoo.com>
2023-04-03 13:11:40 +0000

    About me newspaper style init

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -27,7 +27,40 @@ const Aboutme: React.FC = () => {
           <span className="text-green-400">Technical Architect</span>
         </p>
 
-        <p data-scroll className="text-lg tracking-wide mt-4">
+        <div className="flex flex-row flex-wrap items-start justify-start mt-4">
+          <div className="news-paragraph">
+            <p>
+            Hello! My name is Baskaran, and I am a software developer with more than 20+ years of experience. Currently based in <strong>Singapore</strong>, I completed my master's degree in computer applications from MK University in India.
+            </p>
+          </div>
+
+          <div className="news-paragraph">
+            <p>
+            I have a strong passion for web and mobile development and am self-motivated, results-oriented, and an excellent individual contributor. I have extensive experience working in distributed teams and understand the nuances of working in complex organizations.
+            </p>
+          </div>
+
+          <div className="news-paragraph">
+            <p>
+            Over the years, I have had the opportunity to work with various organizations like Standard Chartered Bank, Deutsche Bank (HCL), Bank of America, and several IT firms, which have been instrumental in honing my skills. Through these experiences, I have learned a lot and met some very helpful people in the industry.
+            </p>
+          </div>
+
+          <div className="news-paragraph">
+            <p>
+            With a proven track record of many successful projects, I have played multiple roles such as full-stack web developer, frontend developer, backend developer, technical architect, and more. This has enabled me to develop a unique set of skills that enables me to deliver a high-quality product while managing complex challenges effectively.
+            </p>
+          </div>
+
+          <div className="news-paragraph">
+            <p>
+            As a software developer, I am passionate about creating innovative solutions to solve complex problems. I am constantly seeking to expand my skill set and keep up with the latest trends and technologies. If you are looking for a dedicated and experienced software developer to join your team or collaborate on a project, I would love to hear from you!
+            </p>
+          </div>
+
+        </div>
+
+        {/* <p data-scroll className="text-lg tracking-wide mt-4">
           I am a software developer with 20+ years of experience and live in{" "}
           <strong>Singapore</strong>. <br />
           <br />I have a <strong>
@@ -57,7 +90,7 @@ const Aboutme: React.FC = () => {
           As a result, I have combined skills that enable me to execute diverse
           projects, delivering a quality product and effectively manage complex
           challenges.
-        </p>
+        </p> */}
       </div>
     </div>
   );
